9fcaad099c88ca6923c4dd62888912fd7e5d0821
eeveenax <eveliagilparedes@gmail.com>
2025-03-24 19:35:30 +0100

    cambiosEve1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
+/**
+ * @author Evelia Gil Paredes
+ * @author Maria Ceballos Mesías
+ */
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        boolean seguir = true;
+
+        while (seguir) {
+
+            System.out.println("Introduce una opción:" + "\n1.Seleccionar Carpeta" + "\n2.Lectura de Fichero"
+                    + "\n3.Conversión Fichero" + "\nCulaquir otra opción te sacará del programa");
+            int opcion = Integer.parseInt(sc.nextLine());
+            switch (opcion) {
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                default:
+                    seguir = false;
+                    break;
+            }
+        }
+
+        sc.close();
     }
 }
