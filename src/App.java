276bc203c5df96b7fd73b8ed82743dcc3a7386f8
eeveenax <eveliagilparedes@gmail.com>
2025-03-24 19:37:06 +0100

    camios

--- src/App.java
+++ src/App.java
@@ -13,7 +13,7 @@ public class App {
         while (seguir) {
 
             System.out.println("Introduce una opción:" + "\n1.Seleccionar Carpeta" + "\n2.Lectura de Fichero"
-                    + "\n3.Conversión Fichero" + "\nCulaquir otra opción te sacará del programa");
+                    + "\n3.Conversión Fichero" + "\nCualquier otra opción te sacará del programa");
             int opcion = Integer.parseInt(sc.nextLine());
             switch (opcion) {
                 case 1:
